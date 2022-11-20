40552b73aa0a58892015b8a410e24af2e73fdc7d
CrackMayo <j.tamayoo@uniandes.edu.co>
2022-11-19 22:46:56 -0500

    Se ajusta el Web API y el Worker para trabajar con Cloud Pub/Sub

--- flaskr/vistas/vistas.py
+++ flaskr/vistas/vistas.py
@@ -12,24 +12,23 @@ from sqlalchemy.exc import IntegrityError
 from flask import jsonify
 from werkzeug.utils import secure_filename
 import time
-from celery import Celery
 from modelos import db, User, UserSchema, Task, EnumTaskStatus, TaskSchema
 from flask import send_from_directory,send_file
 from sqlalchemy import asc,desc
 from google.cloud import storage
+from google.cloud import pubsub_v1
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/home/crackmayo/Desktop/MISW4204-202215-CloudConversionTool-Grupo20/flaskr/vistas/ServiceKey_GoogleCloud.json'
 storage_client = storage.Client()
 
-celery_app = Celery(__name__, broker='redis://localhost:6379/0')
+publisher = pubsub_v1.PublisherClient()
+topic_path = 'projects/desarrollosoftwarenubegrupo20/topics/files_processing'
+
 user_schema = UserSchema()
 task_schema = TaskSchema()
 
-@celery_app.task(name='convert_file')
-def convert_file(*args):
-    pass
-
 def upload_to_bucket(blob_name, file_path, bucket_name):
+
     bucket = storage_client.get_bucket(bucket_name)
     blob = bucket.blob(blob_name)
     blob.upload_from_filename(file_path)
@@ -37,24 +36,25 @@ def upload_to_bucket(blob_name, file_path, bucket_name):
     return blob
 
 def blob_exists(bucket_name, filename):
+
    bucket = storage_client.get_bucket(bucket_name)
    blob = bucket.blob(filename)
    return blob.exists()
 
 def delete_blob(bucket_name, blob_name):
+
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(blob_name)
     blob.delete()
 
 def generate_download_signed_url(bucket_name, blob_name):
+
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(blob_name)
 
     url = blob.generate_signed_url(
         version="v4",
-        # This URL is valid for 5 minutes
         expiration=datetime.timedelta(minutes=5),
-        # Allow GET requests using this URL.
         method="GET",
     )
     return url
@@ -62,7 +62,7 @@ def generate_download_signed_url(bucket_name, blob_name):
 class VistaSignUp(Resource):
 
     def post(self):
-        
+
         if (User.query.filter(User.username == request.json["username"]).first() == None):
             if (User.query.filter(User.email == request.json["email"]).first() == None):
                 if (request.json["password1"] == request.json["password2"]):
@@ -78,10 +78,10 @@ class VistaSignUp(Resource):
         else:
             return 'Intente registrarse con un nombre de usuario diferente!', 400
 
-
 class VistaLogIn(Resource):
 
     def post(self):
+
         user = User.query.filter(User.username == request.json["username"],
                                            User.password == request.json["password"]).first()
         db.session.commit()
@@ -91,10 +91,11 @@ class VistaLogIn(Resource):
             token_de_acceso = create_access_token(identity=user.id)
             return {"mensaje": "Inicio de sesión exitoso!", "token": token_de_acceso}
 
-
 class VistaTasks(Resource):
+
     @jwt_required()
     def post(self):
+
         new_format = request.form["newFormat"].lower()
         audio_file = request.files["fileName"]
         original_format = audio_file.filename.split(".")[-1]
@@ -126,20 +127,34 @@ class VistaTasks(Resource):
                     db.session.commit()
                     user = User.query.filter_by(id=id_user).first()
                     user_email = user.email
-                    args = (new_task.id, new_format, user_folder, filename, original_format, user_email, id_user)                  
-                    convert_file.apply_async(args=args, queue = 'tasks')
+                
+                    data = 'New file to process!'
+                    data = data.encode('utf-8')
+                    attributes = {
+                        'new_task_id': str(new_task.id),
+                        'new_format': str(new_format),
+                        'user_folder': str(user_folder),
+                        'filename': str(filename),
+                        'original_format': str(original_format),
+                        'user_email': str(user_email),
+                        'id_user': str(id_user)
+                    }
+
+                    publisher.publish(topic_path, data, **attributes)
+
                     return task_schema.dump(new_task)
             else:
                 return "El formato del nuevo archivo no es soportado por la plataforma. Solo son validos los formatos mp3, wav y ogg.", 404
         else:
             return "El formato del archivo a convertir no es soportado por la plataforma. Solo son validos los formatos mp3, wav y ogg.", 404
         
-
     @jwt_required()
     def get(self):
+
         id_user = get_jwt_identity()
-        max = request.json.get("max")
-        order = request.json.get("order")
+        max = int(request.args.get('max'))
+        order = int(request.args.get('order'))
+
         if order:
             tasks = Task.query.filter(Task.id_user == id_user).order_by(Task.id.desc()).all()[:max]
         else:
@@ -147,10 +162,10 @@ class VistaTasks(Resource):
  
         return [ task_schema.dump(task) for task in tasks ]
 
-
 class VistaQueue(Resource):
     
     def post(self):
+
         print(request.json["id"])
         try:
             task = Task.query.filter(Task.id == request.json["id"]).first()
@@ -163,12 +178,15 @@ class VistaQueue(Resource):
             return "Bad request!", 400
             
 class VistaTask(Resource):
+
     @jwt_required()
     def get(self,id_task):
+
         return task_schema.dump(Task.query.get_or_404(id_task))
 
     @jwt_required()
     def put(self, id_task):
+
         bucket_name = "cloudconvertertoolstorage"
         task = Task.query.get_or_404(id_task)
         if(task.status == EnumTaskStatus.processed):
@@ -192,14 +210,28 @@ class VistaTask(Resource):
             user = User.query.filter_by(id=id_user).first()
             user_email = user.email
             args = (task.id, task.new_format, user_folder, task.filename, original_format,user_email, id_user)
-            convert_file.apply_async(args=args, queue = 'tasks')
+
+            data = 'New file to process!'
+            data = data.encode('utf-8')
+            attributes = {
+                'new_task_id': str(task.id),
+                'new_format': str(task.new_format),
+                'user_folder': str(user_folder),
+                'filename': str(task.filename),
+                'original_format': str(original_format),
+                'user_email': str(user_email),
+                'id_user': str(id_user)
+            }
+
+            publisher.publish(topic_path, data, **attributes)
+
             return task_schema.dump(task), 200
         else:
             return "La tarea que esta intentado modficar aun no ha sido procesada!", 400
 
-    
     @jwt_required()
     def delete(self,id_task):
+
         bucket_name = "cloudconvertertoolstorage"
         id_user = get_jwt_identity()
         task = Task.query.filter(Task.id == id_task , Task.id_user==id_user).first()
@@ -229,17 +261,14 @@ class VistaTask(Resource):
                 return "La tarea no ha sido procesada aun!",400
 
 class VistaTaskFiles(Resource):
+
     @jwt_required()
     def get(self,filename):
+
         bucket_name = "cloudconvertertoolstorage"
-        id_user=get_jwt_identity()
+        id_user = get_jwt_identity()
         blob_name = 'files/' + str(id_user) + '/' + filename
         if(blob_exists(bucket_name, blob_name)):  
             return generate_download_signed_url(bucket_name, blob_name)
         else:
             return "Archivo no existe!",400
-
-
-
-
-
